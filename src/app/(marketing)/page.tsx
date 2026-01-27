fd5e6b2ba7ad76200b10b8cec9c9d4df0195b7c2
Jorge Matos <45945081+taleex@users.noreply.github.com>
2026-01-27 11:48:21 +0000

    third party components

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,7 +1,29 @@
+import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[#5DC9A8] min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10">
+      <Image src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png" alt="Preview of PetSoft" width={519} height={472}/>
+
+      <div className="flex flex-col">
+        <Logo />
+        <h1 className="text-5xl font-semibold my-6 max-w-[500px]">
+          Manage your <span className="font-extrabold">pet daycare</span> with ease
+        </h1>
+        <p className="text-2xl font-medium max-w-[600px]">
+          Use PetSoft to easily keep track of pets under your care. Get lifetime access for $299.
+        </p>
+        <div className="mt-10 space-x-3">
+          <Button>
+            Get started
+          </Button>
+          <Button variant="secondary">
+            Login
+          </Button>
+        </div>
+      </div>
     </main>
   );
 }
