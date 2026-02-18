a4d7692327b4bf2f8cf1ba713550f40473bbed94
Jorge Matos <45945081+taleex@users.noreply.github.com>
2026-02-18 11:29:18 +0000

    dynamic dialog

--- src/components/pet-button.tsx
+++ src/components/pet-button.tsx
@@ -1,6 +1,8 @@
 import { PlusIcon } from "lucide-react";
 import { Button } from "./ui/button"
 import { cn } from "@/lib/utils";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
+import PetForm from "./pet-form";
 
 type PetButtonProps = {
     actionType: "add" | "edit" | "checkout";
@@ -10,16 +12,28 @@ type PetButtonProps = {
 }
 export default function PetButton({actionType, children, className, onClick}: PetButtonProps) {
 
-    if (actionType === "add"){
-        return <Button className={cn(className)} size="icon" ><PlusIcon className="h-6 w-6"/></Button>
-    }
-
-    if (actionType === "edit"){
-      return <Button className={cn(className)} variant="secondary" >{children}</Button>
-    }
-
-    if (actionType === "checkout"){
+  if (actionType === "checkout"){
       return <Button className={cn(className)} variant="secondary" onClick={onClick}>{children}</Button>
     }
 
-}
+        return (
+          <Dialog>
+            <DialogTrigger asChild >
+              {
+                actionType === "add" ? (
+                  <Button className={cn(className)} size="icon" ><PlusIcon className="h-6 w-6"/></Button>
+                ) : (
+                  <Button className={cn(className)} variant="secondary" >{children}</Button>
+                )
+              }
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>{actionType === "add" ? ("Add New Pet") : ("Edit Pet")}</DialogTitle>
+              </DialogHeader>
+              <PetForm />
+            </DialogContent>
+          </Dialog>
+      );
+    }
+
